c48a2f3a4064b1bea87b9dff33c190982f1d031b
Lilian Roller <lroller@teledara.de>
2016-07-28 17:28:25 +0200

    Continue creating the json file

--- ffbsee.nodes.pl
+++ ffbsee.nodes.pl
@@ -13,6 +13,7 @@ our $json_ffbsee;
 our $ffcommunity = "Freifunk Bodensee";
 our $ffnodes_link = "https://vpn3.ffbsee.de/ffbsee.json";
 our $currentTime = `date +%Y-%m-%dT%H:%M:%S+02:00`;
+our $debug;
 chomp $currentTime;
 our $version = "0.1";
 
@@ -20,8 +21,14 @@ while (my $arg = shift @ARGV) {
     #Komandozeilenargumente: #print "$arg\n";
     if (($arg eq "-h") or ($arg eq "h") or ($arg eq "--help")){
         print "Dieses Script generiert ein freifunk-karte.de kompatibles JSON mit allen FFBSee Nodes\n";
-        exit(0);
+        print "\n\n\t--debug\t Debuging\n";
+		print "\n";
+		exit(0);
     }
+	if ($arg eq "--debug"){
+	    $debug = "True";
+	}
+	
 }
 
 
@@ -34,17 +41,40 @@ close (DATEI);
 our $json_text = $daten;
 our $json = JSON->new->utf8; #force UTF8 Encoding
 our $ffbsee_json = $json->decode( $json_text ); #decode nodes.json
-
+$version = $ffbsee_json->{"version"};
 #	Generiert das JSON:
 $json_ffbsee .= "\{\n\t\"community\": \{\n\t\t\"name\": \"$ffcommunity\",\n\t\t\"href\": \"$ffnodes_link\"\n\t\},\n";
 $json_ffbsee .= "\t\"nodes\": \[\n";
+#
 #	Generate FFNodes
+#
+my $hashref_ffbsee = $ffbsee_json->{"nodes"};
+for my $ffkey (keys %{$hashref_ffbsee}) {
+    if ($debug) { print "$ffkey\n"; }
+    $json_ffbsee .= "\t\t\{\n";
+    $json_ffbsee .= "\t\t\t\"id\": \"$ffkey\"\n";
+	my $ffNodeName = $ffbsee_json->{"nodes"}->{"$ffkey"}->{"nodeinfo"}->{"hostname"};
+    $json_ffbsee .= "\t\t\t\"name\": \"$ffNodeName\"\n";
+	my $ffNodeType;
+	if (( $ffbsee_json->{"nodes"}->{"$ffkey"}->{"nodeinfo"}->{"software"}->{"firmware"}->{"release"} eq "server" )){
+	$ffNodeType = "Gateway";
+	} else {$ffNodeType = "AccessPoint";}
+    $json_ffbsee .= "\t\t\t\"node_type\": \"$ffNodeType\"\n";
+    $json_ffbsee .= "\t\t\t\"position\": \{\n";
+	$json_ffbsee .= "\t\t\t\t\"position\": \{\n";
+	$json_ffbsee .= "\t\t\t\t\"position\": \{\n";
+    $json_ffbsee .= "\t\t\t\},\n";
+	$json_ffbsee .= "\t\t\t\"node_type\": \"$ffNodeType\"\n";
+    $json_ffbsee .= "\t\t\},\n";
+}
 
-
+#
 #	EOFFNodes
+#
 $json_ffbsee .= "\t\],\n\t\"updated_at\": \"$currentTime\",\n\t\"version\": \"$version\"\n\}";
 #	Öffne eine Datei und generiere das JSON
 
 open (DATEI, "> $json_export") or die $!;
-   print DATEI $json_ffbsee;
+    print DATEI $json_ffbsee;
+   
 close (DATEI);
